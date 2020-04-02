6fc87952506a394880959608d2aaa8df496f529e
Elliott Conway <elliottconway@Elliotts-MacBook-Pro.local>
2020-04-02 14:07:57 -0400

    GraphQL Attached

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const graphqlHttp = require('express-graphql');
+const mongoose = require('mongoose');
+
+const { buildSchema } = require('graphql')
+
+// Initialize Port and App
 let port = process.env.PORT;
 if (port == null || port == "") {
     port = 8000;
@@ -8,6 +14,7 @@ if (port == null || port == "") {
 
 const app = express();
 
+// JSON Parser
 app.use(bodyParser.json());
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -20,6 +27,33 @@ app.use((req, res, next) => {
 
     next();
 })
+
+// Attach GraphQL
+app.use('/', graphqlHttp({
+    schema: buildSchema(`
+        type UserInput {
+            password: String!
+        }
+
+        type AuthData {
+            userId: ID!
+            token: String!
+            tokenExpiration: Int!
+        }
+
+        type RootQuery {
+            login(password: String!): AuthData!
+        }
+
+        schema {
+            query: RootQuery
+        }
+    `),
+    rootValue: {
+
+    },
+    graphiql: true,
+}));
 
 app.get('/', (req, res) => res.send('Parish Digital backend'));
 
