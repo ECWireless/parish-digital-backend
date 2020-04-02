4dd77f4a5bbb50edff53e4f47234e678977fbdfb
Elliott Conway <elliottconway@Elliotts-MacBook-Pro.local>
2020-04-02 16:28:31 -0400

    bcrypt added

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const graphqlHttp = require('express-graphql');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-const { buildSchema } = require('graphql')
+const { buildSchema } = require('graphql');
+
+// Password Encryption and Token
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 // Initialize Port and App
 let port = process.env.PORT;
@@ -14,43 +18,68 @@ if (port == null || port == "") {
 
 const app = express();
 
-// JSON Parser
 app.use(bodyParser.json());
+
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
 	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
-    if (req.method === 'OPTIONS') {
-        return res.sendStatus(200);
-    }
+	if (req.method === 'OPTIONS') {
+		return res.sendStatus(200);
+	}
 
-    next();
+	next();
 })
 
 // Attach GraphQL
-app.use('/', graphqlHttp({
+app.use('/graphql', graphqlHttp({
     schema: buildSchema(`
-        type UserInput {
+        type User {
+            _id: ID!
             password: String!
         }
-
         type AuthData {
             userId: ID!
             token: String!
             tokenExpiration: Int!
         }
+        input UserInput {
+            password: String!
+        }
 
         type RootQuery {
             login(password: String!): AuthData!
         }
+        type RootMutation {
+            createUser(userInput: UserInput): User
+        }
 
         schema {
             query: RootQuery
+            mutation: RootMutation
         }
     `),
     rootValue: {
+        login: async({ password }) => {
+            if (password !== 'secret') {
+                throw new Error('Password is incorrect');
+            }
 
+            const token = jwt.sign({userId: 1371113, username: "ParishDigital"}, 'Elkey5819', {
+                expiresIn: '1h'
+            });
+            
+            return {
+                userId: "ParishDigital",
+                token: token,
+                tokenExpiration: 1
+            };
+        },
+        createUser: (args) => {
+            const userInput = args.userInput;
+            return userInput;
+        }
     },
     graphiql: true,
 }));
